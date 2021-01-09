93504094763d564b4f3ff5ac21c39ce5af00ff0d
JMJ Riyana <jeemariyana@gmail.com>
2021-01-09 23:01:10 +0530

    updated home cover

--- js/animation-ball.js
+++ js/animation-ball.js
@@ -1,7 +1,7 @@
 // Some random colors
 const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
 
-const numBalls = 75;
+const numBalls = 50;
 const balls = [];
 
 for (let i = 0; i < numBalls; i++) {
